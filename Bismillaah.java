df472ae5286cfea22e05c17a8b3d2b329efbccc3
Mansoor Taqi Ahmed  |  منصور تقي أحمد <114058186+MansoorTaqiAhmed@users.noreply.github.com>
2023-12-04 14:13:26 +0000

    Save

---
+++ Bismillaah.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Bismillaah
+{
+    public static void main(String args[])
+    {
+        System.out.println("Bismillaahir_Rahmaanir_Raheem_");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a String");
+        String something = scanner.nextLine();
+
+        System.out.println("String: " + something);
+
+    }
+}
